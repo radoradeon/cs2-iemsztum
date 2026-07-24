4cc8a71da35de04c762440192c45ba5e4d7fef36
hirski.radoslaw <hirski.radoslaw@sonija.cloud>
2026-07-24 14:41:34 +0200

    Aktualizacja routingu channels

--- routes/channels.php
+++ routes/channels.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Broadcast;
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
+
+Broadcast::channel('lobby.{id}', function ($user, $lobbyId) {
+    return \App\Models\LobbyPlayer::where('lobby_id', $lobbyId)
+        ->where('user_id', $user->id)
+        ->exists();
+});
